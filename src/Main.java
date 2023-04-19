be1dab56259f99fbdf97592dcc3da778b5235a89
Federico Londoño <felova21@gmail.com>
2023-04-19 09:45:04 -0500

    First commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int sumnumbers = sumnumbers(5, 8, 9);
+        System.out.println(sumnumbers);
+    }
+
+    public static int sumnumbers(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+}
+
